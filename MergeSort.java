1eac7cad56f24edbfa60404ae28259d714184df1
s-jvenable <71246347+s-jvenable@users.noreply.github.com>
2021-01-11 09:24:22 -0800

    initialization

---
+++ MergeSort.java
@@ -0,0 +1,33 @@
+public class MergeSort {
+	
+	/*
+	 * This method takes an array of integers and performs mergesort on the array.
+	 * 
+	 */
+	public static void sort(int[] nums){
+		System.out.println("sorting " + Arrays.toString(nums));
+		// mergeSort left
+		// mergeSort right
+		// merge left and right
+		System.out.println("merging " + Arrays.toString(left) +  " and " + Arrays.toString(right));
+	}
+	
+	/*
+	*	This method takes two sorted arrays, left and right,
+	*   and the original unsorted array. We want to consistently 
+	*   sort in place.
+	*/
+	private static void merge(int[] left, int[] right, int[] nums){
+		
+	}
+	
+	/*
+	*	This method takes a single array, and an inclusive start
+	*	and exclusive stop index. It returns the correct portion
+	*	of the original array. 
+	*/
+	private static int[] splitArray(int[] nums, int start, int stop)
+	{
+		return null;
+	}
+}
---
+++ SortTester.java
@@ -0,0 +1,17 @@
+
+public class SortTester {
+	public static void main(String[] args) {
+		 
+	}
+	
+	/*
+	*	This method takes two integers size and n, 
+	*	and returns an array of random integers from -n to n
+	*   inclusive. 
+	*/
+	public static int[] randomArray(int size, int n)
+	{
+		return null;
+	}
+
+}

6c84b93689b73ad2fb068aa56914aeb33c0af6d5
s-jvenable <71246347+s-jvenable@users.noreply.github.com>
2021-01-14 09:51:55 -0800

    implementation quicksort

---
+++ QuickSort.java
@@ -0,0 +1,47 @@
+public class QuickSort {
+	public static void sort(int[] nums){
+	}
+		// use the private helper methods here. 
+	}
+	
+	/*
+	*
+	* This private helper method takes a single array, an inclusive low, and an inclusive
+	* high. The result is a sorted version of the original array performed
+	* in place.
+	*
+	*/
+	
+	private static void quickSort(int[] nums, int low, int high)
+	{
+		//base case
+		return;
+	
+		//partition nums
+		
+		System.out.println("sorting " + Arrays.toString(nums));
+	
+		//quick sort right
+		//quick sort left
+	
+	}
+	
+	/* 
+	*
+	* This method takes a single array, an inclusive low, and an inclusive high.
+	* It returns the index of the pivot in the array.
+	*
+	*/
+	private static int partition(int[] nums, int low, int high)
+	{
+		//choose pivot (last element)
+	
+		//smaller than pivot on left, larger than pivot on right
+	
+		//place the pivot in correct spot
+	
+		//return index of pivot
+		return 0;
+	}
+}
+
